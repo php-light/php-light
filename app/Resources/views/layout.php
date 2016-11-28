2c81e9b414906c1fc323fdbc1a828b72ab23b880
Khalid Sookia <khalidsookia@gmail.com>
2016-11-28 19:30:45 +0100

    updated skeleton and removed layout

--- app/Resources/views/layout.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by iKNSA.
- * Author: Khalid Sookia <khalidsookia@gmail.com>
- * Date: 28/11/16
- * Time: 18:28
- */
--- index.php
+++ index.php
@@ -11,5 +11,3 @@ require_once "app/config/parameters.php";
 require_once "app/config/routing.php";
 
 require_once "app/AppKernel.php";
-
-require_once "app/Ressources/views/layout.php";
