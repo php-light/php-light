0ab70aead3bf191bb621cd854380f39879aa3c86
Khalid Sookia <khalidsookia@gmail.com>
2016-11-28 18:30:26 +0100

    initialised repository

---
+++ app/Config/Parameters.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by iKNSA.
+ * Author: Khalid Sookia <khalidsookia@gmail.com>
+ * Date: 28/11/16
+ * Time: 18:28
+ */
+
+$parameters = array(
+    "siteRoot" => "index.php",
+    "email" => "khalidsookia@gmail.com"
+);
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by iKNSA.
+ * Author: Khalid Sookia <khalidsookia@gmail.com>
+ * Date: 28/11/16
+ * Time: 18:28
+ */
+
+require_once "app/config/parameters.php";
+
+require_once "app/config/routing.php";
+
+require_once "app/AppKernel.php";
+
+require_once "app/Ressources/views/layout.php";
