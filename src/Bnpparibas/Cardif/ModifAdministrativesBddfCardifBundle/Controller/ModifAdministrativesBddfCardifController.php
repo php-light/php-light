c7fe3d3e939d4e55b769f415d0ad366440129d28
Khalid Sookia <khalidsookia@gmail.com>
2016-12-02 00:54:51 +0100

    Started testing controller

--- src/Bnpparibas/Cardif/ModifAdministrativesBddfCardifBundle/Controller/ModifAdministrativesBddfCardifController.php
+++ src/Bnpparibas/Cardif/ModifAdministrativesBddfCardifBundle/Controller/ModifAdministrativesBddfCardifController.php
@@ -9,12 +9,22 @@
 namespace Bnpparibas\Cardif\ModifAdministrativesBddfCardifBundle\Controller;
 
 
+use Romenys\Framework\Components\UrlGenerator;
 use Romenys\Framework\Controller\Controller;
 
 class ModifAdministrativesBddfCardifController extends Controller
 {
+    private $urlGenerator;
+
+    public function __construct()
+    {
+    }
+
     public function homepageAction($request)
     {
+        $this->urlGenerator = new UrlGenerator($request);
         dump($request);
+        dump($this->urlGenerator->relative("homepage"));
+        dump($this->urlGenerator->absolute("form"));
     }
 }
